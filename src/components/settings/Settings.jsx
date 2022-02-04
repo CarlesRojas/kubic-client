e17d6517395e44bae18aa0353db0c6396e47f3a8
Carles Rojas <carlesrojas@outlook.com>
2022-02-04 17:09:04 +0100

    Settings

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -1,3 +1,70 @@
+import { useState, useCallback, useContext, useEffect } from "react";
+import cn from "classnames";
+import SVG from "react-inlinesvg";
+
+import useAutoResetState from "../../hooks/useAutoResetState";
+
+import { API } from "../../contexts/API";
+import { Utils } from "../../contexts/Utils";
+
+import Logo from "../../resources/icons/tetris.svg";
+
 export default function Settings() {
-    return <div className="Settings"></div>;
+    const { logout, deleteAccount } = useContext(API);
+    const { vibrate } = useContext(Utils);
+
+    // #################################################
+    //   LOGOUT
+    // #################################################
+
+    const [logoutClicked, setLogoutClicked] = useAutoResetState(false, 2000);
+
+    const handleLogoutClicked = useCallback(() => {
+        vibrate(40);
+
+        if (logoutClicked) logout();
+        else setLogoutClicked(true);
+    }, [vibrate, logoutClicked, logout, setLogoutClicked]);
+
+    // #################################################
+    //   DELETE ACCOUNT
+    // #################################################
+
+    const [deleteAccountClicked, setDeleteAccountClicked] = useAutoResetState(false, 2000);
+
+    const handleDeleteAccountClicked = useCallback(() => {
+        vibrate(40);
+
+        if (deleteAccountClicked) deleteAccount();
+        else setDeleteAccountClicked(true);
+    }, [vibrate, deleteAccountClicked, deleteAccount, setDeleteAccountClicked]);
+
+    // #################################################
+    //   RENDER
+    // #################################################
+
+    return (
+        <div className="Settings">
+            <div className="contentContainer">
+                <SVG className="logo" src={Logo}></SVG>
+                <h1>{"KUBIC"}</h1>
+
+                <div className={cn("button", "middle", { low: !logoutClicked })} onClick={handleLogoutClicked}>
+                    LOG OUT
+                </div>
+
+                <div className={cn("button", { low: !deleteAccountClicked })} onClick={handleDeleteAccountClicked}>
+                    DELETE ACCOUNT
+                </div>
+
+                <div className={cn("message", { visible: logoutClicked && !deleteAccountClicked })}>
+                    Log out of this account. Click again to confirm.
+                </div>
+
+                <div className={cn("message", { visible: deleteAccountClicked })}>
+                    Your account and your high score will be deleted. Click again to confirm.
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/contexts/API.jsx
+++ src/contexts/API.jsx
@@ -2,6 +2,7 @@ import { createContext, useContext } from "react";
 import ls from "local-storage";
 
 import { Data } from "./Data";
+import { Events } from "./Events";
 
 const API_VERSION = "api_v1";
 const API_URL = "https://kubic3d.herokuapp.com/"; // "http://localhost:3100/";
@@ -9,6 +10,7 @@ const API_URL = "https://kubic3d.herokuapp.com/"; // "http://localhost:3100/";
 export const API = createContext();
 const APIProvider = (props) => {
     const { APP_NAME, token } = useContext(Data);
+    const { emit } = useContext(Events);
 
     const loginOrRegister = async (username, password) => {
         const postData = { username, password };
@@ -94,9 +96,9 @@ const APIProvider = (props) => {
         }
     };
 
-    const getTopThree = async () => {
+    const getTopTen = async () => {
         try {
-            const rawResponse = await fetch(`${API_URL}${API_VERSION}/getTopThree`, {
+            const rawResponse = await fetch(`${API_URL}${API_VERSION}/getTopTen`, {
                 method: "get",
                 headers: {
                     Accept: "application/json, text/plain, */*",
@@ -111,26 +113,6 @@ const APIProvider = (props) => {
             return response;
         } catch (error) {
             return { error: "Error getting top three" };
-        }
-    };
-
-    const getPeopleAroundYou = async () => {
-        try {
-            const rawResponse = await fetch(`${API_URL}${API_VERSION}/getPeopleAroundYou`, {
-                method: "get",
-                headers: {
-                    Accept: "application/json, text/plain, */*",
-                    "Content-Type": "application/json",
-                    "Access-Control-Allow-Origin": "*",
-                    token: token.current,
-                },
-            });
-
-            const response = await rawResponse.json();
-
-            return response;
-        } catch (error) {
-            return { error: "Error getting people around you" };
         }
     };
 
@@ -164,15 +146,45 @@ const APIProvider = (props) => {
         }
     };
 
+    const logout = () => {
+        ls.clear();
+        token.current = null;
+        emit("refreshApp");
+    };
+
+    const deleteAccount = async () => {
+        try {
+            const rawResponse = await fetch(`${API_URL}${API_VERSION}/deleteAccount`, {
+                method: "get",
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*",
+                    token: token.current,
+                },
+            });
+
+            const response = await rawResponse.json();
+
+            // Logout
+            if ("success" in response) logout();
+
+            return response;
+        } catch (error) {
+            return { error: "Error getting people around you" };
+        }
+    };
+
     return (
         <API.Provider
             value={{
                 loginOrRegister,
                 getUserInfo,
                 setHighScore,
-                getTopThree,
-                getPeopleAroundYou,
+                getTopTen,
                 setTutorialStatus,
+                logout,
+                deleteAccount,
             }}
         >
             {props.children}
